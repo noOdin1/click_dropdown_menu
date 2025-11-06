3fe8397084166c33667ba6e2a2e52041f7e7157f
noOdin1 <218098565+noOdin1@users.noreply.github.com>
2025-11-06 22:46:12 +0800

    list of items to import from element_creator.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
 /* click_dropdown_menu - index.js */
 import "./style.css";
+
+import {
+  createP,
+  createA,
+  createDiv,
+  createButton,
+  createImg,
+  createForm,
+  createFieldset,
+  createLegend,
+  createLabel,
+  createInput,
+  createSelect,
+  createOption,
+  htmlOps,
+} from "./element_creator.js";
