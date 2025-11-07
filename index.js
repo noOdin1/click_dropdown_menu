18413d114a032da07803c163823a34b8a0155d1e
noOdin1 <218098565+noOdin1@users.noreply.github.com>
2025-11-08 03:46:19 +0800

    Import list from element_creator.js

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 /* click_dropdown_menu - index.js */
 
+import {
+  createP,
+  createA,
+  createDiv,
+  createButton,
+  createImg,
+  createForm,
+  createFieldset,
+  createLegend,
+  createLabel,
+  createInput,
+  createSelect,
+  createOption,
+} from "./element_creator.js";
